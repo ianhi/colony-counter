35de00e5f93a78c79dbd44445be5b0336eb8333b
noahhi <noahhuntisaak@gmail.com>
2020-04-22 02:41:42 -0400

    Add circle draw on click + circle counter

--- src/main.ts
+++ src/main.ts
@@ -1,45 +1,42 @@
 class DrawingApp {
   private canvas: HTMLCanvasElement;
   private context: CanvasRenderingContext2D;
-  private paint: boolean;
   private img: HTMLImageElement;
 
   private clickX: number[] = [];
   private clickY: number[] = [];
-  private clickDrag: boolean[] = [];
+
+  private colonyCount: number = 0;
+  private colonyCountDisplay: HTMLElement;
 
   constructor() {
     const canvas = document.getElementById('canvas') as HTMLCanvasElement;
     const context = canvas.getContext('2d');
     context.lineCap = 'round';
     context.lineJoin = 'round';
-    context.strokeStyle = 'black';
-    context.lineWidth = 1;
+    context.strokeStyle = 'red';
+    context.lineWidth = 2;
 
     this.canvas = canvas;
     this.context = context;
+
+    this.colonyCountDisplay = document.getElementById('colonyCounter');
 
     this.img = new Image();
     this.img.src = 'colonies.png';
     this.img.onload = () => {
       this.drawImageScaled();
-      //      this.context.drawImage(this.img, 0, 0);
     };
+
     this.redraw();
     this.createUserEvents();
   }
+
   private createUserEvents() {
     const canvas = this.canvas;
 
     canvas.addEventListener('mousedown', this.pressEventHandler);
-    canvas.addEventListener('mousemove', this.dragEventHandler);
-    canvas.addEventListener('mouseup', this.releaseEventHandler);
-    canvas.addEventListener('mouseout', this.cancelEventHandler);
-
     canvas.addEventListener('touchstart', this.pressEventHandler);
-    canvas.addEventListener('touchmove', this.dragEventHandler);
-    canvas.addEventListener('touchend', this.releaseEventHandler);
-    canvas.addEventListener('touchcancel', this.cancelEventHandler);
 
     document
       .getElementById('clear')
@@ -66,50 +63,40 @@ class DrawingApp {
       img.height * ratio
     );
   }
+
   private redraw() {
     const clickX = this.clickX;
-    const context = this.context;
-    //context.drawImage(this.img, 0, 0);
-    const clickDrag = this.clickDrag;
     const clickY = this.clickY;
+    const context = this.context;
+
     for (let i = 0; i < clickX.length; ++i) {
       context.beginPath();
-      if (clickDrag[i] && i) {
-        context.moveTo(clickX[i - 1], clickY[i - 1]);
-      } else {
-        context.moveTo(clickX[i] - 1, clickY[i]);
-      }
-
-      context.lineTo(clickX[i], clickY[i]);
-      context.strokeStyle = '#ff0000';
+      context.arc(clickX[i],clickY[i], 10, 0, 2*Math.PI);
       context.stroke();
     }
+
     context.closePath();
   }
 
-  private addClick(x: number, y: number, dragging: boolean) {
+  private addClick(x: number, y: number) {
     this.clickX.push(x);
     this.clickY.push(y);
-    this.clickDrag.push(dragging);
+    this.colonyCount++;
+    this.updateCounterDisplay();
+  }
+
+  private updateCounterDisplay() {
+     this.colonyCountDisplay.innerHTML = "Colony Count : " + this.colonyCount.toString();
   }
 
   private clearCanvas() {
     this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.clickX = [];
     this.clickY = [];
-    this.clickDrag = [];
   }
+
   private clearEventHandler = () => {
     this.clearCanvas();
-  };
-
-  private releaseEventHandler = () => {
-    this.paint = false;
-    this.redraw();
-  };
-
-  private cancelEventHandler = () => {
-    this.paint = false;
   };
 
   private pressEventHandler = (e: MouseEvent | TouchEvent) => {
@@ -122,27 +109,8 @@ class DrawingApp {
     mouseX -= this.canvas.offsetLeft;
     mouseY -= this.canvas.offsetTop;
 
-    this.paint = true;
-    this.addClick(mouseX, mouseY, false);
+    this.addClick(mouseX, mouseY);
     this.redraw();
-  };
-
-  private dragEventHandler = (e: MouseEvent | TouchEvent) => {
-    let mouseX = (e as TouchEvent).changedTouches
-      ? (e as TouchEvent).changedTouches[0].pageX
-      : (e as MouseEvent).pageX;
-    let mouseY = (e as TouchEvent).changedTouches
-      ? (e as TouchEvent).changedTouches[0].pageY
-      : (e as MouseEvent).pageY;
-    mouseX -= this.canvas.offsetLeft;
-    mouseY -= this.canvas.offsetTop;
-
-    if (this.paint) {
-      this.addClick(mouseX, mouseY, true);
-      this.redraw();
-    }
-
-    e.preventDefault();
   };
 }
 
