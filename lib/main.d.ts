9ad4f215e3f5fe6aaf82a4c361834ca8577cd5ca
ianhi <ianhuntisaak@gmail.com>
2022-01-20 06:23:24 +0000

    Deploying to gh-pages from @ ianhi/colony-counter@b55949ded97dc6ef908ce4facd6ba721685d23fa 🚀

--- lib/main.d.ts
+++ lib/main.d.ts
@@ -1,16 +1,18 @@
+declare function arrayToCsv(data: number[][]): string;
 declare class DrawingApp {
     private imgCanvas;
     private pointCanvas;
     private imgContext;
     private pointContext;
     private img;
-    private clickX;
-    private clickY;
+    private pointRadius;
+    private clicks;
     private colonyCount;
     private colonyCountDisplay;
     constructor();
     private createUserEvents;
     private drawImageScaled;
+    private redrawPoints;
     private drawPoint;
     private addClick;
     private updateCounterDisplay;
